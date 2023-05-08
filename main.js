0bd407d2b2a69f96fb9d0715281389f99d3a8d65
Oscar Ascencio <oscareascencioalva@gmail.com>
2023-05-07 22:13:57 -0600

    Erros

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const menuItems = document.querySelectorAll(".navresponsive");
 
 boton.addEventListener("click", mostrarMenu);
 quitar.addEventListener("click", quitarMenu);
-menuItems[0].addEventListener("click", quitarMenu);
-menuItems[1].addEventListener("click", quitarMenu);
-menuItems[2].addEventListener("click", quitarMenu);
+menuItems[0].addEventListener("click", quitarMenu2);
+menuItems[1].addEventListener("click", quitarMenu2);
+menuItems[2].addEventListener("click", quitarMenu2);
 
 
 function mostrarMenu(e) {
@@ -20,6 +20,12 @@ function mostrarMenu(e) {
 
 function quitarMenu(e) {
     e.preventDefault();
+    menu.classList.remove("menuabajo");
+    menu.classList.add("menuarriba");
+
+}
+
+function quitarMenu2(e) {
     menu.classList.remove("menuabajo");
     menu.classList.add("menuarriba");
 
